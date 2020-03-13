14008ae08be2702452d0e5101412b0c2ae80f016
usr1 <usr1@me.com>
2020-03-13 16:48:15 +0530

    UI changes

--- buzz/generator.py
+++ buzz/generator.py
@@ -6,7 +6,7 @@ buzz = ('continuous testing', 'continuous integration',
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
-verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts', "Srinivas charan@CDC2 1023AM 7thJan20")
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts', "Srinivascharan@HDC2 446PM 13thMarch20")
 
 def sample(l, n = 1):
     result = random.sample(l, n)
