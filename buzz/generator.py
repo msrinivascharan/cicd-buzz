85b91ab0d0098c69061055f5eae3b90cb319dc6f
msrinivascharan <msrinivascharan@gmail.com>
2020-12-15 16:42:10 +0530

    Update generator.py

--- buzz/generator.py
+++ buzz/generator.py
@@ -6,7 +6,7 @@ buzz = ('continuous testing', 'continuous integration',
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
-verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts', "srinivas.c.mamidi-42511102020")
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts', "srinivas.c.mamidi-44115122020")
 
 def sample(l, n = 1):
     result = random.sample(l, n)
